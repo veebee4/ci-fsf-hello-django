b740d50981f294f82a1fe776a9157e4147d69f06
Varinia Butcher <varinia_b@hotmail.com>
2024-09-15 17:05:45 +0000

    Add allowed host to settings file

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-veebee4-cifsfhellodjang-x4xivjosdl9.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['veebee4-django-todo-app-fc258033d439.herokuapp.com']
 
 
 # Application definition
